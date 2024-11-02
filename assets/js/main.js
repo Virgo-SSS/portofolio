a3f1342f193c549d13f95b75bed84951fe0854e1
virgo <virgostevanus27@gmail.com>
2024-11-02 15:39:49 +0700

    add portofolio v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,34 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+// Check if the user has a theme preference in local storage
+const theme = localStorage.getItem('theme');
+if (theme) {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    if (theme === 'dark') {
+        document.getElementById('themeSwitch').checked = true;
+        document.querySelector('.form-check-label').innerHTML = '<i class="bx bx-moon"></i>';
+    }
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+// Function to toggle between light and dark modes
+function toggleMode() {
+    const htmlElement = document.documentElement;
+    const themeSwitchLabel = document.querySelector('.form-check-label');
+    const isChecked = document.getElementById('themeSwitch').checked;
+
+    if (isChecked) {
+        // Switch to dark mode
+        htmlElement.setAttribute('data-bs-theme', 'dark');
+        themeSwitchLabel.innerHTML = '<i class="bx bx-moon"></i>';
+        // Save the theme preference in local storage
+        localStorage.setItem('theme', 'dark');
+    } else {
+        // Switch to light mode
+        htmlElement.setAttribute('data-bs-theme', 'light');
+        themeSwitchLabel.innerHTML = '<i class="bx bx-sun"></i>';
+        // Save the theme preference in local storage
+        localStorage.setItem('theme', 'light');
+    }
 }
 
-
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
-
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-
-/*=============== ADD BLUR TO HEADER ===============*/
-const blurHeader = () =>{
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('blur-header') 
-                       : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
-
-
-/*=============== EMAIL JS ===============*/
-const contactForm = document.getElementById('contact-form'),
-    contactMessage = document.getElementById('contact-message')
-
-const sendEmail = (e) => {
-    e.preventDefault()
-
-        emailjs.sendForm('service_vher9bk', 'template_x6fz32n', '#contact-form', '8Tu_TvPuv2VOxpIGp')
-        .then(() => {
-            contactMessage.textContent ='Your message has been sent successfully ✅'
-
-            setTimeout(() => {
-                contactMessage.textContent =''
-            }, 5000)
-
-            contactForm.reset()
-        }, () => {
-            contactMessage.textContent ='Your message has not been sent ❌'
-        })
-}
-
-contactForm.addEventListener('submit', sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
 const scrollUp = () =>{
 	const scrollUp = document.getElementById('scroll-up')
@@ -71,39 +37,3 @@ const scrollUp = () =>{
 						: scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
-
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
-}
-window.addEventListener('scroll', scrollActive)
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal ({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    reset: true // animations repeat
-})
-
-sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`)
-sr.reveal(`.home__imag`, {origin: 'bottom'})
-sr.reveal(`.about__data, .skills__data`, {origin: 'left'})
-sr.reveal(`.about__image, .skills__content`, {origin: 'right'})
-sr.reveal(`.services__card, .projects__card`, {interval: 100})
